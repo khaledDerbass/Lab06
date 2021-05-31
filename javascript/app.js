0f88cf6220e0d9d19eb12236c93cfef17f99809b
Khaled <derbasskhaled1@gmail.com>
2021-05-31 11:30:07 +0300

    to push Salmoon cookies list

--- javascript/app.js
+++ javascript/app.js
@@ -3,15 +3,21 @@
 
 var hours = ['6 AM ', '7 AM', '8 AM ', '9 AM', '10 AM ', '11 AM', '12 PM ', '1 PM', '2 PM ', '3 PM', '4 PM ', '5 PM', '6 PM ', '7 PM'];
 
-var parent= document.getElementById('salesTable');
-var articleElement=document.createElement('article');
-var ulElement = document.createElement('ul');
-articleElement.appendChild(ulElement);
-parent.appendChild(articleElement);
+
 //parent.appendChild(ulElement);
 
 function showTable(locObj) 
 {
+
+    var parent= document.getElementById('salesTable');
+var articleElement=document.createElement('article');
+var ulElement = document.createElement('ul');
+articleElement.appendChild(ulElement);
+    let name = document.createElement('h3');
+      
+    name.textContent = locObj.nameObj;
+
+    parent.appendChild(name);
 
     for (let x = 0; x < hours.length; x++) {
 
@@ -23,10 +29,11 @@ function showTable(locObj)
         ulElement.appendChild(liElement);
     }
     let liElement2 = document.createElement('li');
-      
-    liElement2.textContent = 'Total : '+ locObj.total+' <br>';
-
     ulElement.appendChild(liElement2);
+    liElement2.textContent = 'Total : '+ locObj.total;
+
+    parent.appendChild(articleElement);
+
 }
 
 
